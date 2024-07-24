624f78ee730656ec75f8f803ed8126f8e16ebcfc
Yash Gupta <yasharsh.28@gmail.com>
2024-07-24 22:24:25 +0530

    smoothening map

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -34,6 +34,7 @@ public class MapGenerator : MonoBehaviour
     {
         AllocateMapMemory();
         FillMapData();
+        SmoothenMap();
     }
 
     void AllocateMapMemory()
@@ -56,18 +57,48 @@ public class MapGenerator : MonoBehaviour
                 else
                 {
                     int randomNum = Random.Range(0, 100);
-                    if (randomNum < fillPercent)
-                    {
-                        map[i, j] = 1;
-                    }
-                    else
-                    {
-                        map[i, j] = 0;
-                    }
+                    map[i, j] = (randomNum < fillPercent) ? 1 : 0;
                 }
                 
             }
         }
+    }
+
+    void SmoothenMap()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1)
+                {
+                    continue;
+                }
+                int wallNeighbors = CountWallNeighbors(i,j);
+                if (wallNeighbors > 4)
+                {
+                    map[i, j] = 1;
+                }
+                else if (wallNeighbors < 4)
+                {
+                    map[i, j] = 0;
+                }
+            }
+        }
+    }
+
+    int CountWallNeighbors(int i, int j)
+    {
+        int count = 0;
+        if (map[i - 1, j - 1] == 1) count++;
+        if (map[i - 1, j] == 1) count++;
+        if (map[i - 1, j + 1] == 1) count++;
+        if (map[i, j - 1] == 1) count++;
+        if (map[i, j + 1] == 1) count++;
+        if (map[i + 1, j - 1] == 1) count++;
+        if (map[i + 1, j] == 1) count++;
+        if (map[i + 1, j + 1] == 1) count++;
+        return count;
     }
 
     void OnDrawGizmos()
